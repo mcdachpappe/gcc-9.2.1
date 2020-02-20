2ec050bceb009625d8d4e20dec5ba254ecd3e77a
mcdachpappe <noreference@web.de>
2020-02-20 19:16:21 +0100

    Initial commit
    * import gcc 9.2.1
    
    Signed-off-by: mcdachpappe <noreference@web.de>

---
+++ lib/gcc/aarch64-linux-gnu/9.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --enable-languages=c,c++,lto --with-isl --enable-lto --enable-plugin --target=aarch64-linux-gnu --prefix=/home/danny/eas/Toolchains/build-tools-gcc/aarch64-linux-gnu --disable-multilib --disable-werror CFLAGS='-g0 -O3 -fstack-protector-strong' CXXFLAGS='-g0 -O3 -fstack-protector-strong'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
